25c2d3701f22d3b0ed56d62e97158fe1d8b9ffdd
Egor Grin <35704842+egorgrin@users.noreply.github.com>
2023-11-23 17:07:30 +0500

    Mobile menu enhanced

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,6 @@
 const header = document.querySelector('header');
 const mobileNav = document.querySelector('.mobile_nav');
+const mobileNavBody = document.querySelector('.mobile_nav_body');
 const burgerBtn = document.querySelector('.hamb');
 
 burgerBtn.addEventListener('click', toggleMenu);
@@ -13,8 +14,12 @@ function toggleMenu() {
   mobileNav.classList.toggle('opened');
   if (mobileNav.classList.contains('opened')) {
     mobileNav.style.maxHeight = `${mobileNavHeight}px`;
-    mobileNav.style.transform = `translateY(${headerHeight}px)`;
+    mobileNav.style.transform = `translate(-50%, ${headerHeight}px)`;
+    mobileNavBody.style.transform = `translateY(0)`;
+
   } else {
+    mobileNavBody.style.transform = `translateY(-25%)`;
+
     mobileNav.style.maxHeight = '0px';
     mobileNav.style.transform = '0px';
     setTimeout(() => {
